a6c6bd3f0f15cd508b607fff34a72306c2c99f1d
Logan <cb58logan@gmail.com>
2023-08-17 21:48:15 +0100

    test CSS fix 3  (oops)

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        superCoolEdgyPurple: "#9e75f0",
+        deepPurple:"#17101a",
+        shallowPurple:"#31233a",
+        puddlePurple:"#433151",
+        contrastingBlue:"#53caf5",
+        fadedRed:"#c8464a",
+      }
+    },
+  },
+  plugins: [
+  ],
+};
+
+module.exports = config;
+
